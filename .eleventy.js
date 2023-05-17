a437b8d5704d9e93d4bfc63f73c59c6ea367a3b7
chriskirknielsen <chriskirknielsen@gmail.com>
2023-05-17 08:30:31 -0400

    Add toLocaleUrl filter proof of concept

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,16 @@
 const { EleventyI18nPlugin } = require('@11ty/eleventy');
+const defaultLanguage = 'en';
 
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPlugin(EleventyI18nPlugin, {
-		defaultLanguage: 'en',
+		defaultLanguage: defaultLanguage,
 		errorMode: 'allow-fallback',
+	});
+	eleventyConfig.addFilter('toLocaleUrl', function (baseSlug) {
+		const locale = this?.ctx?.locale || this?.ctx?.page?.lang || defaultLanguage;
+		const data = this.ctx[baseSlug][locale];
+		const localisedSlug = data.slug;
+		return `/${locale}/${localisedSlug}`;
 	});
 
 	return {
